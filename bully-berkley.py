5066d7305d8162636ba464251972fad89da0cbdf
Rodrigo Calábria <rodrigovcalabria@hotmail.com>
2018-06-25 00:42:24 -0300

    Berkley teminado

--- bully-berkley.py
+++ bully-berkley.py
@@ -7,10 +7,12 @@ import netifaces
 import time
 import pickle
 from threading import Thread
+from random import randint
 
 
 INICIA_ELEICAO = 10
 RESPOSTA_ELEICAO = 11
+LIDER_ATUAL = 20
 
 GRUPO_MC = '224.0.0.0'
 PORTA = 8888
@@ -18,14 +20,8 @@ PID = str(os.getpid())
 
 isLeader = False
 currentLeaderAddr = ''
-
-mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-mySocket.bind(('', PORTA))
-mreq = struct.pack('4sL', socket.inet_aton(GRUPO_MC), socket.INADDR_ANY)
-mySocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
-myAddr = netifaces.ifaddresses(netifaces.interfaces()[1])[2][0]['addr']
-print(myAddr)
+timeList = []
+currentTime = 0
 
 
 class Mensagem():
@@ -33,23 +29,6 @@ class Mensagem():
 		self.action = action
 		self.msg = msg
 
-
-while True:
-	print("Esperando mensagem")
-	readables, writeables, exceptions = select.select([mySocket, sys.stdin], [], [mySocket])
-	for sock in readables:
-		if sock == mySocket:
-			serial_data, sender_addr = mySocket.recvfrom(512)
-			if sender_addr[0] == myAddr:
-				continue
-
-			received_data = pickle.loads(serial_data)
-
-		elif sock == sys.stdin:
-			inicia_eleicao(mySocket)
-
-	print("Aperte enter para iniciar a eleição.")
-	sys.stdin.readline()
 
 """
 ALGORITMO DO BULLY
@@ -60,28 +39,29 @@ def start_election():
 
 	mySocket.sendto(serial_data, (GRUPO_MC, PORTA))
 
-	timeout_mark = time.time() + 1000
+	timeoutMark = time.time() + 1.0
 	while True:
-		readables, writeables, exceptions = select.select([mySocket], [], [mySocket], timeout_mark - time.time())
-
-		received_data = receiveMessage()
-
-		if received_data[0] == RESPOSTA_ELEICAO:
-			isLeader = False
-			break
-
-		if (timeout_mark - time.time()) <= 0:
+		timeOut = timeoutMark - time.time()
+		if timeOut >= 0:
+			readables, writeables, exceptions = select.select([mySocket], [], [mySocket], timeOut)
+		else:
 			print("Torna-se lider.")
 			isLeader = True
 			currentLeaderAddr = myAddr
-			sendLeader()
+			send_leader()
+			break
+
+		received_data = receive_message()
+
+		if received_data[0] == RESPOSTA_ELEICAO:
+			isLeader = False
 			break
 
 
 """
 GERENCIA MENSAGENS
 """
-def receiveMessage():
+def receive_message():
 	serial_data, sender_addr = mySocket.recvfrom(512)
 	received_data = pickle.loads(serial_data)
 
@@ -95,21 +75,101 @@ def receiveMessage():
 
 			return (INICIA_ELEICAO, False)
 
-		""" para RESPOSTA_ELEICAO """
 		elif received_data.action == RESPOSTA_ELEICAO:
 			return (RESPOSTA_ELEICAO, True)
 
-		""" para DEFINE_LIDER """
-		elif received_data.action == DEFINE_LIDER:
+		elif received_data.action == LIDER_ATUAL:
 			currentLeaderAddr = received_data.msg
-			return (DEFINE_LIDER, True)
+			return (LIDER_ATUAL, True)
+
+		elif received_data.action == INICIA_BERKLEY:
+			adjust = received_data - currentTime
+			response = Mensagem(RESPOSTA_BERKLEY, adjust)
+			serial_response = pickle.dumps(response)
+			mySocket.sendto(serial_response, sender_addr)
+			return (INICIA_BERKLEY, True)
+
+		elif received_data.action == RESPOSTA_BERKLEY:
+			timeList.append((sender_addr, received_data.msg))
+			return (RESPOSTA_BERKLEY, True)
+
+		elif received_data.action == AJUSTE_BERKLEY:
+			currentTime = received_data.msg + currentTime
 
 	return (None, None)
 
 """
 ANUNCIA LIDER
 """
-def sendLeader():
-	msg = Mensagem(DEFINE_LIDER, myAddr)
+def send_leader():
+	msg = Mensagem(LIDER_ATUAL, myAddr)
 	serial_data = pickle.dumps(msg)
 	mySocket.sendto(serial_data, (GRUPO_MC, PORTA))
+
+
+"""
+BERKLEY
+"""
+def run_berkley():
+	msg = Mensagem(INICIA_BERKLEY, currentTime)
+	serial_data = pickle.dumps(msg)
+	mySocket.sendto(serial_data, (GRUPO_MC, PORTA))
+
+	timeoutMark = time.time() + 1.0
+	while True:
+		timeOut = timeoutMark - time.time()
+		if timeOut >= 0:
+			readables, writeables, exceptions = select.select([mySocket], [], [mySocket], timeOut)
+		else:
+			break
+
+		receive_message()
+
+	timeSum = 0
+	for _, time in timeList:
+		timeSum += int(time)
+
+	timeAvg = int(timeSum / (len(timeList) + 1))
+
+	for addr, time in timeList:
+		timeAdjust = timeAvg - int(time)
+		msg = Mensagem(AJUSTE_BERKLEY, timeAdjust)
+		serial_data = pickle.dumps(msg)
+		mySocket.sendto(serial_data, addr)
+	currentTime += timeAvg
+	timeList.clear()
+
+
+def start_clock():
+	currentTime = 0
+	timeStep = randint(1,5)
+	# timeStep = 1
+	while True:
+		currentTime += timeStep
+		time.sleep(0.25)
+
+
+
+""" MAIN """
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+mySocket.bind(('', PORTA))
+mreq = struct.pack('4sL', socket.inet_aton(GRUPO_MC), socket.INADDR_ANY)
+mySocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+myAddr = netifaces.ifaddresses(netifaces.interfaces()[1])[2][0]['addr']
+print('IP: ', myAddr)
+
+Thread(target = start_clock).start()
+
+while True:
+	print("Aperte enter para iniciar a eleição.")
+	readables, writeables, exceptions = select.select([mySocket, sys.stdin], [], [mySocket], 0)
+	print('passou')
+	for sock in readables:
+		if sock == mySocket:
+			receive_message()
+
+		elif sock == sys.stdin:
+			sys.stdin.readline()
+			start_election()
