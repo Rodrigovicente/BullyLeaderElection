a35a4f55559483391cb23a81fdb8c41bcc876fbb
Rodrigo Calábria <rodrigovcalabria@hotmail.com>
2018-06-25 02:00:25 -0300

    algoritmo completo

--- bully-berkley.py
+++ bully-berkley.py
@@ -36,54 +36,65 @@ ALGORITMO DO BULLY
 def start_election():
 	msg = Mensagem(INICIA_ELEICAO, PID)
 	serial_data = pickle.dumps(msg)
-
+	print('Enviando para todos em multicast.')
 	mySocket.sendto(serial_data, (GRUPO_MC, PORTA))
 
+	print('Esperando respostas.')
 	timeoutMark = time.time() + 1.0
 	while True:
 		timeOut = timeoutMark - time.time()
-		if timeOut >= 0:
+		if timeOut > 0:
 			readables, writeables, exceptions = select.select([mySocket], [], [mySocket], timeOut)
 		else:
-			print("Torna-se lider.")
+			print('Timout: não houve resposta. Torna-se lider.')
 			isLeader = True
 			currentLeaderAddr = myAddr
 			send_leader()
 			break
 
-		received_data = receive_message()
+		if readables:
+			received_data = receive_message()
 
-		if received_data[0] == RESPOSTA_ELEICAO:
-			isLeader = False
-			break
+			if received_data[0] == RESPOSTA_ELEICAO:
+				print('Não é lider. Há um PID maior.')
+				isLeader = False
+				break
 
 
 """
 GERENCIA MENSAGENS
 """
 def receive_message():
+	print('Lendo mensagem...', end="", flush=True)
 	serial_data, sender_addr = mySocket.recvfrom(512)
 	received_data = pickle.loads(serial_data)
 
 	if sender_addr[0] != myAddr:
+		print('\n')
 		""" para INICIA_ELEICAO """
 		if received_data.action == INICIA_ELEICAO:
+			print('Pedido de eleição recebido.')
 			if PID > int(received_data.msg):
+				print('\t- Tem PID maior (', PID, ' > ', int(received_data.msg), '). Enviando resposta.')
 				serial_response = pickle.dumps(RESPOSTA_ELEICAO)
 				mySocket.sendto(serial_response, sender_addr)
 				return (INICIA_ELEICAO, True)
 
+			print('\t- Tem PID menor (', PID, ' < ', int(received_data.msg), '). NAO envia nada.')
 			return (INICIA_ELEICAO, False)
 
 		elif received_data.action == RESPOSTA_ELEICAO:
+			print('Rebendo resposta de eleição.')
 			return (RESPOSTA_ELEICAO, True)
 
 		elif received_data.action == LIDER_ATUAL:
+			print('Definindo novo lider para: ', received_data.msg)
 			currentLeaderAddr = received_data.msg
 			return (LIDER_ATUAL, True)
 
 		elif received_data.action == INICIA_BERKLEY:
-			adjust = received_data - currentTime
+			adjust = int(received_data.msg) - currentTime
+			print('Pedido de valor de ajuste para o algoritom Berkley. Ajuste enviado: ', adjust)
 			response = Mensagem(RESPOSTA_BERKLEY, adjust)
 			serial_response = pickle.dumps(response)
 			mySocket.sendto(serial_response, sender_addr)
@@ -96,12 +107,18 @@ def receive_message():
 		elif received_data.action == AJUSTE_BERKLEY:
 			currentTime = received_data.msg + currentTime
 
-	return (None, None)
+		else:
+			print('Padrão de mensagem não reconhecido.')
+			return None
+	else:
+		print(' loopback.')
+		return (None, None)
 
 """
 ANUNCIA LIDER
 """
 def send_leader():
+	print('Enviando mensagem em multicast anunciando novo lider.')
 	msg = Mensagem(LIDER_ATUAL, myAddr)
 	serial_data = pickle.dumps(msg)
 	mySocket.sendto(serial_data, (GRUPO_MC, PORTA))
@@ -118,7 +135,7 @@ def run_berkley():
 	timeoutMark = time.time() + 1.0
 	while True:
 		timeOut = timeoutMark - time.time()
-		if timeOut >= 0:
+		if timeOut > 0:
 			readables, writeables, exceptions = select.select([mySocket], [], [mySocket], timeOut)
 		else:
 			break
@@ -136,7 +153,7 @@ def run_berkley():
 		msg = Mensagem(AJUSTE_BERKLEY, timeAdjust)
 		serial_data = pickle.dumps(msg)
 		mySocket.sendto(serial_data, addr)
-	currentTime += timeAvg
+	currentTime += timeAvg 
 	timeList.clear()
 
 
@@ -157,19 +174,20 @@ mySocket.bind(('', PORTA))
 mreq = struct.pack('4sL', socket.inet_aton(GRUPO_MC), socket.INADDR_ANY)
 mySocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-myAddr = netifaces.ifaddresses(netifaces.interfaces()[1])[2][0]['addr']
+list_target = 1
+myAddr = netifaces.ifaddresses(netifaces.interfaces()[list_target])[2][0]['addr']
 print('IP: ', myAddr)
 
 Thread(target = start_clock).start()
 
 while True:
-	print("Aperte enter para iniciar a eleição.")
-	readables, writeables, exceptions = select.select([mySocket, sys.stdin], [], [mySocket], 0)
-	print('passou')
+	print('Aperte enter para iniciar a eleição.')
+	readables, writeables, exceptions = select.select([mySocket, sys.stdin], [], [mySocket])
 	for sock in readables:
 		if sock == mySocket:
 			receive_message()
 
 		elif sock == sys.stdin:
 			sys.stdin.readline()
+			print('Iniciando eleição.')
 			start_election()
